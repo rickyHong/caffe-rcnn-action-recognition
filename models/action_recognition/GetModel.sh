0c8f1a63f62efda8a4fe5a5c060909b70a04aaac
Xuanyi Dong <dongxuanyi888@icloud.com>
2016-05-01 13:13:35 +0800

    Add action rec model

---
+++ models/action_recognition/GetModel.sh
@@ -0,0 +1,4 @@
+wget http://mmlab.siat.ac.cn/pretrain/vgg_16_action_flow_pretrain.caffemodel
+wget http://mmlab.siat.ac.cn/pretrain/vgg_16_action_rgb_pretrain.caffemodel
+wget http://mmlab.siat.ac.cn/very_deep_two_stream_model/cuhk_action_spatial_vgg_16_split1.caffemodel
+wget http://mmlab.siat.ac.cn/very_deep_two_stream_model/cuhk_action_temporal_vgg_16_split1.caffemodel
