b5375491e8843a39abdfce2f2e59045ed5fb40b5
Xuanyi Dong <dongxuanyi888@icloud.com>
2016-05-02 12:51:43 +0800

    Fix

--- src/caffe/ACTION_REC/video_data_layer.cpp
+++ src/caffe/ACTION_REC/video_data_layer.cpp
@@ -32,7 +32,12 @@ void VideoDataLayer<Dtype>:: DataLayerSetUp(const vector<Blob<Dtype>*>& bottom, 
     string filename;
     int label;
     int length;
+    int zero_count = 0;
     while (infile >> filename >> length >> label){
+        if (length == 0){
+            zero_count ++;
+            continue;
+        }
         lines_.push_back(std::make_pair(filename,label));
         lines_duration_.push_back(length);
     }
@@ -43,7 +48,7 @@ void VideoDataLayer<Dtype>:: DataLayerSetUp(const vector<Blob<Dtype>*>& bottom, 
         ShuffleVideos();
     }
 
-    LOG(INFO) << "A total of " << lines_.size() << " videos.";
+    LOG(INFO) << "A total of " << lines_.size() << " videos.  zeros frames videos " << zero_count;
     lines_id_ = 0;
 
     Datum datum;
@@ -112,18 +117,22 @@ void VideoDataLayer<Dtype>::load_batch(Batch<Dtype>* batch){
         CHECK_GT(lines_size, lines_id_);
         vector<int> offsets;
         int average_duration = (int) lines_duration_[lines_id_] / num_segments;
-        LOG(ERROR) << "VideoData : load_batch [] " << item_id << " / " << batch_size << "  .. num_segments : " << num_segments;
+        while (average_duration == 0){
+            LOG(WARNING) << "VideoData : load_batch [] " << item_id << " / " << batch_size << "  .. num_segments : " << num_segments << ",  ave : " << average_duration << " = " << lines_[lines_id_].first;
+            Next_Line_Id();
+            average_duration = (int) lines_duration_[lines_id_] / num_segments;
+        }
         for (int i = 0; i < num_segments; ++i){
             if (this->phase_==TRAIN){
                 caffe::rng_t* frame_rng = static_cast<caffe::rng_t*>(frame_prefetch_rng_->generator());
                 int offset = (*frame_rng)() % (average_duration - new_length + 1);
-                LOG(ERROR) << "VideoData : load_batch [] offset : " << offset << ", average_duration : " << average_duration << ", " << new_length;
+                //LOG(ERROR) << "VideoData : load_batch [] offset : " << offset << ", average_duration : " << average_duration << ", " << new_length;
                 offsets.push_back(offset+i*average_duration);
             } else{
                 offsets.push_back(int((average_duration-new_length+1)/2 + i*average_duration));
             }
         }
-
+        //LOG(ERROR) << "VideoData : " << (this->phase_==TRAIN?"TRAIN":"TEST") << "　 ---After num_seg";
         if (this->layer_param_.video_data_param().modality() == VideoDataParameter_Modality_FLOW){
             if(!ReadSegmentFlowToDatum(lines_[lines_id_].first, lines_[lines_id_].second, offsets, new_height, new_width, new_length, &datum)) {
                 LOG(WARNING) << "Load Flow Data Failed, " << item_id << "/" << batch_size << " batch : ( " << lines_[lines_id_].first << " ) [" << lines_[lines_id_].second << "]" 
@@ -142,19 +151,24 @@ void VideoDataLayer<Dtype>::load_batch(Batch<Dtype>* batch){
 
         int offset1 = batch->data_.offset(item_id);
     	this->transformed_data_.set_cpu_data(top_data + offset1);
-        LOG(ERROR) << "Transform : Before";
         this->data_transformer_->Transform(datum, &(this->transformed_data_));
-        LOG(ERROR) << "Transform : After";
+        //LOG(ERROR) << "Transform : After";
         top_label[item_id] = lines_[lines_id_].second;
 
         //next iteration
-        lines_id_++;
-        if (lines_id_ >= lines_size) {
-            DLOG(INFO) << "Restarting data prefetching from start.";
-            lines_id_ = 0;
-            if(this->layer_param_.video_data_param().shuffle()){
-                ShuffleVideos();
-            }
+        Next_Line_Id();
+    }
+}
+
+template <typename Dtype>
+void VideoDataLayer<Dtype>::Next_Line_Id(){
+    lines_id_++;
+    const int lines_size = lines_.size();
+    if (lines_id_ >= lines_size) {
+        DLOG(INFO) << "Restarting data prefetching from start.";
+        lines_id_ = 0;
+        if(this->layer_param_.video_data_param().shuffle()){
+            ShuffleVideos();
         }
     }
 }
